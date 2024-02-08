b51e4162b24ab2813ad52f95dd5b880b4d7259a9
Will Castillo <william.castillo.jr@outlook.com>
2024-02-08 13:17:28 -0500

    Created page - validFood function

---
+++ model/validate.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/*
+ * 328/diner/model/validate.php
+ * Validate data for the diner app
+ */
+
+// Return true if food is valid
+function validFood($food)
+{
+    if (trim($food == ""))
+    {
+        return false;
+    }
+    if (!ctype_alpha($food))
+    {
+        return false;
+    }
+    return true;
+}
